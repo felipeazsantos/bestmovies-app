1275ec42aa7e3b2fe19c1a22f52a92b5a0be32e0
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-09-12 17:19:18 -0300

    feat: header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Search from "./components/Search.jsx";
+import {useState} from "react";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
         <main>
             <div className="pattern" />
+            <div className="wrapper">
+                <header>
+                    <img src="/hero.png" alt="" />
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+                </header>
+
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
         </main>
     )
 }
